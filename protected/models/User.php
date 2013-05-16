fbe118ea132e722395dafb3f09908b07a048d6ef
Никита Мельников <n.melnikov@dengionline.com>
2013-05-16 14:49:47 +0400

    Fix: codeStyle в модуле Account и модель User

--- protected/models/User.php
+++ protected/models/User.php
@@ -1,9 +1,5 @@
 <?php
-
 /**
- * This is the model class for table "User".
- *
- * The followings are the available columns in table 'User':
  * @property integer $id
  * @property string $first_name
  * @property string $second_name
@@ -20,60 +16,76 @@ class User extends CActiveRecord
 	const LOGIN = 'login';
 	const PROFILE = 'profile';
 	const PASSWORD = 'password';
-	
-	public $password_repeat;
-		
+
+	/**
+	 * @var string используется для подтверждения пароля
+	 */
+	public $passwordRepeat;
+
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @return User the static model class
 	 */
-	public static function model($className=__CLASS__)
+	public static function model($className = __CLASS__)
 	{
 		return parent::model($className);
 	}
 
-	/**
-	 * @return string the associated database table name
-	 */
 	public function tableName()
 	{
 		return 'users';
 	}
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
-			array('first_name, last_name, email, phone, password, password_repeat', 'required', 'on'=>self::SIGNUP, 'message'=>'Заполните поле {attribute}'),
-			array('first_name, last_name, second_name, zip, address, city, email, phone, website', 'required', 'on'=>self::PROFILE, 'message'=>'Заполните поле {attribute}'),
-			array('email, password', 'required', 'on'=>self::LOGIN, 'message'=>'Заполните поле {attribute}'),
-			array('email, phone', 'unique', 'on'=>self::SIGNUP, 'message'=>'Такой {attribute} уже зарегистрирован'),
-			array('email, password', 'length', 'max'=>255, 'on'=>self::SIGNUP, 'message'=>'Максимальная длина {attribute} 255 символов'),
-			array('phone', 'length', 'max'=>20, 'on'=>self::SIGNUP, 'message'=>'{attribute} должен состоять не более 20 цифр'),
-			array('password', 'compare', 'compareAttribute'=>'password_repeat', 'on'=>self::SIGNUP, 'message'=>'Пароли не совпадают'),
-			array('id, email, phone, password', 'safe', 'on'=>'search'),
-			array('email', 'filter', 'filter'=>'mb_strtolower', 'on'=>self::SIGNUP, 'message'=>'{attribute} должен быть записан маленькими буквами'),
+			array(
+				'first_name, last_name, email, phone, password, passwordRepeat',
+				'required',
+				'on' => self::SIGNUP,
+				'message' => 'Заполните поле {attribute}'
+			),
+			array(
+				'first_name, last_name, second_name, zip, address, city, email, phone, website',
+				'required',
+				'on' => self::PROFILE,
+				'message' => 'Заполните поле {attribute}'
+			),
+			array('email, password', 'required', 'on' => self::LOGIN, 'message' => 'Заполните поле {attribute}'),
+			array('email, phone', 'unique', 'on' => self::SIGNUP, 'message' => 'Такой {attribute} уже зарегистрирован'),
+			array(
+				'email, password',
+				'length',
+				'max' => 255,
+				'on' => self::SIGNUP,
+				'message' => 'Максимальная длина {attribute} 255 символов'
+			),
+			array(
+				'phone',
+				'length',
+				'max' => 20,
+				'on' => self::SIGNUP,
+				'message' => '{attribute} должен состоять не более 20 цифр'
+			),
+			array(
+				'password',
+				'compare',
+				'compareAttribute' => 'passwordRepeat',
+				'on' => self::SIGNUP,
+				'message' => 'Пароли не совпадают'
+			),
+			array('id, email, phone, password', 'safe', 'on' => 'search'),
+			array(
+				'email',
+				'filter',
+				'filter' => 'mb_strtolower',
+				'on' => self::SIGNUP,
+				'message' => '{attribute} должен быть записан маленькими буквами'
+			),
+			array('email', 'email', 'on' => self::SIGNUP),
 		);
 	}
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
-
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
 	public function attributeLabels()
 	{
 		return array(
@@ -85,8 +97,8 @@ class User extends CActiveRecord
 			'phone' => 'Телефон',
 			'website' => 'Сайт',
 			'password' => 'Пароль',
-			'password_repeat' => 'Повторите пароль',
-			'photo' => 'Фотография',	
+			'passwordRepeat' => 'Повторите пароль',
+			'photo' => 'Фотография',
 			'address' => 'улица, дом, квартира',
 			'city' => 'город',
 			'zip' => 'почтовый индекс',
@@ -99,60 +111,56 @@ class User extends CActiveRecord
 	 */
 	public function search()
 	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+		$criteria = new CDbCriteria;
 
-		$criteria=new CDbCriteria;
+		$criteria->compare('id', $this->id);
 
-		$criteria->compare('id',$this->id);
+		$criteria->compare('last_name', $this->first_name, true);
+		$criteria->compare('first_name', $this->last_name, true);
+		$criteria->compare('email', $this->email, true);
 
-		$criteria->compare('last_name',$this->first_name,true);
-		$criteria->compare('first_name',$this->last_name,true);
-		$criteria->compare('email',$this->email,true);
+		$criteria->compare('phone', $this->phone, true);
 
-		$criteria->compare('phone',$this->phone,true);
-
-		$criteria->compare('password',$this->password,true);
+		$criteria->compare('password', $this->password, true);
 
 		return new CActiveDataProvider('User', array(
-			'criteria'=>$criteria,
+			'criteria' => $criteria,
 		));
 	}
 
 	protected function beforeSave()
 	{
-		if(parent::beforeSave())
-		{
-			if($this->isNewRecord)
-			{
+		if (parent::beforeSave()) {
+			if ($this->isNewRecord) {
 				$this->registered = date('Y-m-d H:i:s');
-                $this->password = self::hashPassword($this->password);
-			}
-			
-			if($this->scenario == self::PASSWORD)
-			{
 				$this->password = self::hashPassword($this->password);
 			}
-	
+
+			if ($this->scenario == self::PASSWORD) {
+				$this->password = self::hashPassword($this->password);
+			}
+
 			return true;
 		}
-	
+
 		return false;
 	}
-	
+
 	static function hashPassword($password)
 	{
 		return md5($password);
 	}
-	
+
 	public function login()
 	{
 		$identity = new UserIdentity($this->email, $this->password);
-		if($identity->authenticate()) {
+		if ($identity->authenticate()) {
 			Yii::app()->user->login($identity);
+
 			return true;
 		} else {
 			$this->addError('email', 'Не верный email или пароль');
+
 			return false;
 		}
 	}
