f92cb6d24a233efdfb1da8b5d865271c3b859f3d
Aleksey Razbakov <aleksey@razbakov.com>
2013-05-31 14:14:03 +0300

    1.0.3 fixed photo layout

--- protected/modules/society/views/accounts/index.php
+++ protected/modules/society/views/accounts/index.php
@@ -8,7 +8,9 @@ $this->breadcrumbs=array(
 
 <?php foreach ($model as $user): ?>
     <div class="span-1">
-        <img src="<?php echo $user['photo'] ?>" width="200">
+        <div style="height: 200px;overflow: hidden">
+            <img src="<?php echo $user['photo'] ?>" width="200">
+        </div>
         <h4><?php echo $user['first_name'] ?> <?php echo $user['last_name'] ?></h4>
         <p><?php echo $user['email'] ?></p>
         <p><?php echo $user['phone'] ?></p>
