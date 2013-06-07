43f3bc158a844065152456fec6456e4e233b5553
Aleksey Razbakov <aleksey@razbakov.com>
2013-06-08 02:06:59 +0300

    added email confirmation language

---
+++ protected/views/mail/confirmation_ru.php
@@ -0,0 +1,10 @@
+<p>Добро пожаловать в Community Online, <?php echo $user->first_name ?>.</p>
+<p>Для начала работы <a href="http://tvp.cabinet.io/index.php/ru/society/accounts/confirm/<?php echo $user->hash ?>">подтвердите ваш email</a></p>
+
+<br>
+--<br>
+С уважением,<br>
+Алексей Разбаков<br>
+aleksey@razbakov.com<br>
+<br>
+Если будут вопросы - обращайтесь.
